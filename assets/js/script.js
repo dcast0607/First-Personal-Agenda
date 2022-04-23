fdf2b87dd7f55e816ea2860c223a81274dcce012
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-04-23 14:59:26 -0700

    Added logic to render a table that the user can interact with

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,77 @@ function updateTime() {
 }
 //When the document loads, we display the current time and
 //then we call a updateTime function to update our time.
+
+/*
+Need to iterate through the template literal to create all of our elements.
+*/
+function generateAgendaTable () {
+    for (var i=8; i <= 17; i++) {
+        var timeOfDay = "am";
+        var placeHolderText ="Enter your note here:";
+        if (i < 12 ) {
+            var tableElement = `
+                <table class="agendaTable">
+                    <body class="tableBody">
+                        <tr class="defaultTableStyling">
+                            <th class="timeColumn">${i}:00 ${timeOfDay}</th>
+                            <td class="userEventEntry"></td>
+                            <td class="userEntrySaveButton">&#128190 Save</td>
+                        </tr>
+                        <tr class="defaultTableStyling">
+                            <th class="timeColumn">${i}:30 ${timeOfDay}</th>
+                            <td class="userEventEntry"></td>
+                            <td class="userEntrySaveButton">&#128190 Save</td>
+                        </tr>
+                    </body>
+                </table>
+                `;
+        }
+        if (i === 12 ){
+            timeOfDay = "pm";
+            var tableElement = `
+            <table class="agendaTable">
+                <body class="tableBody">
+                    <tr class="defaultTableStyling">
+                        <th class="timeColumn">${i}:00 ${timeOfDay}</th>
+                        <td class="userEventEntry"></td>
+                        <td class="userEntrySaveButton">&#128190 Save</td>
+                    </tr>
+                    <tr class="defaultTableStyling">
+                        <th class="timeColumn">${i}:30 ${timeOfDay}</th>
+                        <td class="userEventEntry"></td>
+                        <td class="userEntrySaveButton">&#128190 Save</td>
+                    </tr>
+                </body>
+            </table>
+            `;
+        }
+        if (i > 12 ) {
+            iConverted = i - 12;
+            timeOfDay = "pm";
+            var tableElement = `
+            <table class="agendaTable">
+                <body class="tableBody">
+                    <tr class="defaultTableStyling">
+                        <th class="timeColumn">${iConverted}:00 ${timeOfDay}</th>
+                        <td class="userEventEntry"></td>
+                        <td class="userEntrySaveButton">&#128190 Save</td>
+                    </tr>
+                    <tr class="defaultTableStyling">
+                        <th class="timeColumn">${iConverted}:30 ${timeOfDay}</th>
+                        <td class="userEventEntry"></td>
+                        <td class="userEntrySaveButton">&#128190 Save</td>
+                    </tr>
+                </body>
+            </table>
+            `;
+        }
+        $('#agendaTable').append(tableElement);
+    }
+}
+
 $(document).ready(function(){
-    displayDate.text(moment().format("MMMM D, YYYY"));
+    displayDate.text("Date: " + moment().format("MMMM D, YYYY"));
     updateTime();
+    generateAgendaTable();
 })
